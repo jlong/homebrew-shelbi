19bf599248b3ce23dec98bb33300effe2f763ee0
John W. Long <john@32pixels.co>
2026-07-18 00:23:31 -0400

    shelbi 0.6.0 (#9)
    Co-authored-by: shelbi-release-bot <shelbi-release-bot@users.noreply.github.com>

--- Formula/shelbi.rb
+++ Formula/shelbi.rb
@@ -1,18 +1,18 @@
 class Shelbi < Formula
   desc "Open-source agent orchestrator built on tmux"
   homepage "https://github.com/jlong/shelbi"
-  version "0.5.0"
+  version "0.6.0"
   license "MIT"
 
   depends_on "tmux"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jlong/shelbi/releases/download/v0.5.0/shelbi_Darwin_arm64.tar.gz"
-      sha256 "94a822dbca95cbcbc4c298851f14955e6acb0d55e0684f63f3b214c2b5139462"
+      url "https://github.com/jlong/shelbi/releases/download/v0.6.0/shelbi_Darwin_arm64.tar.gz"
+      sha256 "18248ab0e7e0dbe081c362018577ece6610dab92d520928cc384f55285deccee"
     else
-      url "https://github.com/jlong/shelbi/releases/download/v0.5.0/shelbi_Darwin_x86_64.tar.gz"
-      sha256 "e1fcd458e257ad1473cd6176969538a3f6088800a907819469ffbfc728aa7e9a"
+      url "https://github.com/jlong/shelbi/releases/download/v0.6.0/shelbi_Darwin_x86_64.tar.gz"
+      sha256 "44f924c38244bc00e65e0a72ae5b85babd3a51c882ba21bf73cbc2a9198daea7"
     end
   end
 
